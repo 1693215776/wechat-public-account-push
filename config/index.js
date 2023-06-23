ae219128541b2cc3466bd3797d37a769e7a3e270
1693215776 <63027561+1693215776@users.noreply.github.com>
2023-06-23 20:35:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const USER_CONFIG = {
     poetry: false,
     
     /** 星座运势 */
-    horoscope: true,
+    horoscope: false,
     
     /** 生日消息和节日消息 */
     birthdayMessage: true,
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKy0U5wE6Du-8hgtcifIhG6nm2Bc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "XhEt7V3qzIg7aIf2TuRV68-OpJoO1lZw_zSwHRhPNVo	",
+      useTemplateId: "XhEt7V3qzIg7aIf2TuRV68-OpJoO1lZw_zSwHRhPNVo",
       // 所在省份
       province: "湖南",
       // 所在城市
