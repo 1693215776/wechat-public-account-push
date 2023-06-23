47bbe2ac3e360c7977e42bccc4cb79548fcd59e6
1693215776 <63027561+1693215776@users.noreply.github.com>
2023-06-23 20:20:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "1",
+  APP_SECRET: "",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -32,7 +32,7 @@ export const USER_CONFIG = {
     // 每日一言
     oneTalk: true,
     // 土味情话(彩虹屁)
-    earthyLoveWords: false,
+    earthyLoveWords: true,
     // 朋友圈文案
     momentCopyrighting: false,
     // 毒鸡汤
@@ -70,7 +70,7 @@ export const USER_CONFIG = {
       // 所在城市
       city: "长沙",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-07',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -78,17 +78,17 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "园宝", "year": "2001", "date": "05-04"},
+        {"type": "*生日", "name": "小霞", "year": "2004", "date": "03-07"},
       
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
    
         {"type": "节日", "name": "万圣节", "year": "2021", "date": "10-31"},
-        {"type": "节日", "name": "见面", "year": "2021", "date": "11-01"},
+        {"type": "节日", "name": "见面", "year": "2023", "date": "06-25"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-04-01"},
+        {"keyword": "love_day", date: "2023-06-21"},
       
       ]
     },
