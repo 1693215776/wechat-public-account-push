5f499cedce3bb21acf5641a01eb38026aa0cf822
1693215776 <63027561+1693215776@users.noreply.github.com>
2023-06-23 22:21:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKy0U5wE6Du-8hgtcifIhG6nm2Bc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "7eghnw4x1tMkoXXmzt7W7-_Vts-Vas2CUSZSaj-Ce_o",
+      useTemplateId: "gK732oPclJblnUKQvXN5i8TuONPl4Ua2TThTN5I-9BA",
       // 所在省份
       province: "湖南",
       // 所在城市
